354bdf134b6e53153530be0958c973b478cf1ec3
Rasmus <Rasmusmberg@gmail.com>
2021-12-29 19:37:22 +0100

    crud for user lavet og begyndene auth og posts begyndene crud

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routers/users");
+const authRoute = require("./routers/auth");
+const postRoute = require("./routers/posts");
+
 
 dotenv.config();
 
 mongoose.connect(
     process.env.MONGO_URL, 
+    { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
     console.log("Connected to MongoDB");
     }
@@ -19,9 +24,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-    res.send("Welcome to LookAtMyFish")
-});
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
