5d19bc6ccd0e570af091fd56a6f453039a4af7ee
kasper <kasperkbf@hotmail.com>
2022-01-08 17:49:16 +0100

    Initial Page - Register

---
+++ public/pages/register/register.js
@@ -0,0 +1,23 @@
+function registerUser(){
+fetch("/api/auth", {
+    method: "POST",
+    headers: { "Content-type": "application/json; charset=UTF-8"},
+    body: JSON.stringify ({
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    })
+}).then(res => {
+    if (res.status == 200) {
+        console.log("Register successfull")
+        setTimeout(() => location.href= "/login", 1500);
+    }
+    else {
+        console.log("Error:", res.status)
+    }
+}) 
+}
+
+
+    document.getElementById("register-user").addEventListener("click", registerUser)
+
--- routers/pages.js
+++ routers/pages.js
@@ -3,11 +3,15 @@ const router = require("express").Router();
 const { createPage } = require("../render.js");
 
 const loginPage = createPage("login/login.html", {title: "Look At My Fish | Login"});
-
+const registerPage = createPage("register/register.html", {title: "Look At My Fish | Register"});
 
 
 router.get("/", (req, res) => {
     res.send(loginPage);
+});
+
+router.get("/register", (req, res) => {
+    res.send(registerPage);
 });
 
 module.exports = router;
